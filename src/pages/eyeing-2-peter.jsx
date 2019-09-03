e909563af4241561462e0b9392d014c94e604c1b
calebmchenry <calebmchenry@gmail.com>
2019-09-02 20:11:53 -0400

    Order eyeing 2 peter

--- src/pages/eyeing-2-peter.jsx
+++ src/pages/eyeing-2-peter.jsx
@@ -22,28 +22,44 @@ export default () => {
 
   const pathToFiles = "content/bible-classes/darkness-to-light/"
 
-  const fileData = results.allFile.edges
+  const overview = results.allFile.edges
+    .filter(({ node }) => node.relativePath.includes("Overview"))
     .map(({ node }) => {
+      const regex = /([\d]*)[A-Z]?\s(.*)\.doc/
+      const matches = node.relativePath.match(regex)
       return {
         href: `${pathToFiles}${node.relativePath}`,
-        name: node.name,
+        name: matches[2],
         id: node.id,
       }
     })
-    .filter(d => d.name.includes("delete-me") === false)
+  const powerPointData = results.allFile.edges
+    .filter(({ node }) => node.relativePath.includes(".ppt"))
+    .map(({ node }) => {
+      const regex = /([\d]*)[A-Z]?\s(.*)\.(pptx|ppt)/
+      const matches = node.relativePath.match(regex)
+      return {
+        href: `${pathToFiles}${node.relativePath}`,
+        number: parseInt(matches[1]),
+        name: matches[2],
+        id: node.id,
+      }
+    })
+    .sort((a, b) => {
+      return a.number - b.number
+    })
 
   return (
     <Layout>
       <SEO title="Eyeing 2 Peter"></SEO>
       <div className="container">
         <h1>Eyeing 2 Peter</h1>
-        {fileData.length === 0 ? (
-          <span>Coming soon...</span>
-        ) : (
-          fileData.map(data => (
-            <DownloadItem key={data.id} {...data}></DownloadItem>
-          ))
-        )}
+        <h2 style={{ fontWeight: 200 }}>Overview</h2>
+        <DownloadItem {...overview[0]}></DownloadItem>
+        <h2 style={{ fontWeight: 200 }}>PowerPoints</h2>
+        {powerPointData.map(data => (
+          <DownloadItem key={data.id} {...data}></DownloadItem>
+        ))}
       </div>
     </Layout>
   )
